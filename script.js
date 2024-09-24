d07f6f4f9d4ddf572c7e22fb0d16c6e155dacc5e
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-24 23:07:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let container=document.querySelector("#grid-container")
+let submit=document.querySelector("#change_button")
+let reset=document.querySelector("#Reset")
+for(let i=0;i<9;i++){
+	let newdiv=document.createElement("div")
+	newdiv.innerText=i+1;
+	newdiv.setAttribute("id", "grid-item-" + (i+1));
+	newdiv.className="same";
+	container.append(newdiv);
+}
+let items=document.querySelectorAll(".same")
+let input=document.querySelector("#block_id")
+let color=document.querySelector("#color_id")
+submit.addEventListener("click",changecolor)
+function changecolor() {
+	let gridnumber=input.value;
+	items[gridnumber-1].style.backgroundColor=color.value		
+}
+reset.addEventListener("click", resetColor);
+function resetColor() {
+    items.forEach(item => {
+        item.style.backgroundColor = "transparent";
+    });
+}
+
+
